8c71f40b7673f97f7916c67577a91e260c50bece
Kunal Patel <kunalp1025@gmail.com>
2018-09-22 14:03:13 -0400

    Add welcome activity
    Add button for login
    Add button for sign up
    
    Add loginOptionpressed method to launch new activity using intent

--- app/src/main/java/team58/cs2340/donationtracker/Welcome.java
+++ app/src/main/java/team58/cs2340/donationtracker/Welcome.java
@@ -1,7 +1,9 @@
 package team58.cs2340.donationtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void onLoginOptionPressed(View view) {
+        //Intent intent = new Intent(this, Login.class);
+        //startActivity(intent);
     }
 }
