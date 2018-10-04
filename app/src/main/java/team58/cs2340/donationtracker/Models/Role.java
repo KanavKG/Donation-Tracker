9771887b5486a4e9b2bd3bcd8a30849fbead18b9
Kunal Patel <kunalp1025@gmail.com>
2018-10-04 16:17:35 -0400

    Add role name to Role enum
    add roleName and constructor

--- app/src/main/java/team58/cs2340/donationtracker/Models/Role.java
+++ app/src/main/java/team58/cs2340/donationtracker/Models/Role.java
@@ -4,7 +4,17 @@ package team58.cs2340.donationtracker.Models;
  * This class represents various user roles
  */
 public enum Role {
-    USER,
-    LOCATIONEMPLOYEE,
-    ADMIN
+    USER("User"),
+    LOCATIONEMPLOYEE("Location Employee"),
+    ADMIN("Admin");
+
+    private String roleName;
+
+    Role (String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return this.roleName;
+    }
 }
