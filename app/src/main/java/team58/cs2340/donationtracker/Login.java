6e1679a1bbf163cc4d92e5d759a3f2de422b9480
Kunal Patel <kunalp1025@gmail.com>
2018-10-04 16:28:13 -0400

    Update login method
    get rid of hardcoded username and password
    use Model singleton to check validLogin

--- app/src/main/java/team58/cs2340/donationtracker/Login.java
+++ app/src/main/java/team58/cs2340/donationtracker/Login.java
@@ -5,30 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import team58.cs2340.donationtracker.Models.Model;
+
 public class Login extends AppCompatActivity {
+
+    private TextView email;
+    private TextView password;
+
+    private Model model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.email = findViewById(R.id.email);
+        this.password = findViewById(R.id.password);
+
+        this.model = Model.getInstance();
+
     }
 
     public void onClick(View v) {
         int id = v.getId();
         switch (id) {
-            case R.id.logauthBtn:
+            case R.id.login:
                 v.clearFocus();
-                EditText username = findViewById(R.id.usernameField);
-                EditText password = findViewById(R.id.passwordField);
-                if (username.getText().toString().equals("user") &&
-                        password.getText().toString().equals("pass")) {
+                if (model.validLogin(this.email.getText().toString(),
+                        this.password.getText().toString())) {
                     Intent intent = new Intent(this, MainActivity.class);
                     startActivity(intent);
                     Toast.makeText(getApplicationContext(), "Welcome",Toast.LENGTH_SHORT).show();
                 } else {
-                    username.setText("");
                     password.setText("");
                     Toast.makeText(getApplicationContext(), "Username and/or password incorrect!",Toast.LENGTH_LONG).show();
                 }
