e0f41c361dc2159216200d903252203519bef8c7
Kunal Patel <kunalp1025@gmail.com>
2018-10-04 16:16:30 -0400

    Update Model Singleton
    fix addUser and validLogin methods

--- app/src/main/java/team58/cs2340/donationtracker/Models/Model.java
+++ app/src/main/java/team58/cs2340/donationtracker/Models/Model.java
@@ -2,13 +2,13 @@ package team58.cs2340.donationtracker.Models;
 
 import java.util.HashMap;
 
-class Model {
+public class Model {
     private static final Model ourInstance = new Model();
 
-    HashMap<String, User> users;
-    User currentUser;
+    private HashMap<String, User> users;
+    private User currentUser;
 
-    static Model getInstance() {
+    public static Model getInstance() {
         return ourInstance;
     }
 
@@ -25,15 +25,18 @@ class Model {
     }
 
     public boolean addUser(String email, User user) {
-        return users.put(user.getEmail(), user) == null;
+        return users.put(user.getEmail(), user) != null;
     }
 
     public boolean validLogin(String email, String password) {
         User user = users.get(email);
         if (user == null) {
             return false;
+        } else if (user.getPassword().equals(password)) {
+            this.currentUser = user;
+            return true;
         } else {
-            return user.getPassword().equals(password);
+            return false;
         }
     }
 }
