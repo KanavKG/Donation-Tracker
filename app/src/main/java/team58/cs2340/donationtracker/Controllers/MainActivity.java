93fcf582381a8dab915ac4d3dc1b25c99dd89dc5
iecurius <saahilyechuri@gmail.com>
2018-10-11 17:36:56 -0400

    csv reader

--- app/src/main/java/team58/cs2340/donationtracker/Controllers/MainActivity.java
+++ app/src/main/java/team58/cs2340/donationtracker/Controllers/MainActivity.java
@@ -6,10 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+
+import team58.cs2340.donationtracker.Models.Location;
+import team58.cs2340.donationtracker.Models.LocationType;
 import team58.cs2340.donationtracker.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private List<Location> locations = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +35,35 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(logoutIntent);
                 Toast.makeText(getApplicationContext(), "Logout Successful!",Toast.LENGTH_SHORT).show();
                 break;
+        }
+    }
+
+    private void readLocationData() {
+        InputStream instream = getResources().openRawResource(R.raw.locationdata);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(instream, Charset.forName("UTF-8")));
+        String ln;
+        try {
+            //skip header line
+            reader.readLine();
+            while ((ln = reader.readLine()) != null) {
+                String[] tokens = ln.split(",");
+                Location loc = new Location();
+                loc.setKey(Integer.parseInt(tokens[0]));
+                loc.setName(tokens[1]);
+                loc.setLatitude(Double.parseDouble(tokens[2]));
+                loc.setLongitude(Double.parseDouble(tokens[3]));
+                loc.setStreetAddress(tokens[4]);
+                loc.setCity(tokens[5]);
+                loc.setState(tokens[6]);
+                loc.setZip(tokens[7]);
+                loc.setType(LocationType.valueOf(tokens[0]));
+                loc.setPhoneNumber(tokens[0]);
+                loc.setWebsite(tokens[0]);
+                locations.add(loc);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- app/src/main/java/team58/cs2340/donationtracker/Models/Location.java
+++ app/src/main/java/team58/cs2340/donationtracker/Models/Location.java
@@ -2,32 +2,117 @@ package team58.cs2340.donationtracker.Models;
 
 public class Location {
 
+    private int key;
     private String name;
-    private LocationType type;
-    private double longitude;
     private double latitude;
-    private String address;
+    private double longitude;
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String zip;
+    private LocationType type;
     private String phoneNumber;
+    private String website;
 
-    public Location(String name, LocationType type, double longitude, double latitude,
-                    String address, String phoneNumber) {
+    public Location() { }
+    public Location(int key, String name, double latitude, double longitude,
+                    String streetAddress, String city, String state,
+                    String zip, LocationType type, String phoneNumber,
+                    String website) {
+        this.key = key;
+
         this.name = name;
-        this.type = type;
-        this.longitude = longitude;
         this.latitude = latitude;
-        this.address = address;
+        this.longitude = longitude;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.type = type;
+        this.phoneNumber = phoneNumber;
+        this.website = website;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public LocationType getType() {
+        return type;
+    }
+
+    public void setType(LocationType type) {
+        this.type = type;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getName() {
-        return this.name;
+    public String getWebsite() {
+        return website;
     }
 
-    public String getAddress() {
-        return this.address;
+    public void setWebsite(String website) {
+        this.website = website;
     }
 
-    public String getPhoneNumber() {
-        return this.phoneNumber;
+    @Override
+    public String toString() {
+        return "Location{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
