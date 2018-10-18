90fa8387ce76b56c4058077433057beb07c93c03
Kunal Patel <kunalp1025@gmail.com>
2018-10-18 17:09:59 -0400

    Continue stubbing AddDonation activity

--- app/src/main/java/team58/cs2340/donationtracker/Models/AddDonation.java
+++ app/src/main/java/team58/cs2340/donationtracker/Models/AddDonation.java
@@ -14,17 +14,24 @@ public class AddDonation extends AppCompatActivity {
     private TextView name;
     private TextView shortDescription;
     private TextView fullDescription;
-    //private add field for monetary value (double)
+    private TextView value;
     private TextView comment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_donation);
-        this.model = Model.getInstance();
+        /*this.model = Model.getInstance();
+        this.name = findViewById(R.id.name);
+        this.shortDescription = findViewById(R.id.shortDescription);
+        this.fullDescription = findViewById(R.id.fullDescription);
+        this.value = findViewById(R.id.value);
+        this.comment = findViewById(R.id.comment);*/
     }
 
     public void onAddClicked(View view) {
-
+        /*Donation donation = new Donation(model.getCurrentUser().getLocation(),
+                name.getText().toString(), shortDescription.getText().toString(),
+                fullDescription.getText().toString())*/
     }
 }
