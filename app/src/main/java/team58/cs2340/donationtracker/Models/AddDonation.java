f4f2542d8c371a67137d8722de5737a9dcba1b2b
Kunal Patel <kunalp1025@gmail.com>
2018-10-18 16:50:42 -0400

    Stub out AddDonation Activity

--- app/src/main/java/team58/cs2340/donationtracker/Models/AddDonation.java
+++ app/src/main/java/team58/cs2340/donationtracker/Models/AddDonation.java
@@ -2,14 +2,29 @@ package team58.cs2340.donationtracker.Models;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import team58.cs2340.donationtracker.R;
 
 public class AddDonation extends AppCompatActivity {
 
+    private Model model;
+
+    private TextView name;
+    private TextView shortDescription;
+    private TextView fullDescription;
+    //private add field for monetary value (double)
+    private TextView comment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_donation);
+        this.model = Model.getInstance();
+    }
+
+    public void onAddClicked(View view) {
+
     }
 }
