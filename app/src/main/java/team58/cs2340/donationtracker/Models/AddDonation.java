d5eee50c28f42e724f01fad786af733881b07717
Kunal Patel <kunalp1025@gmail.com>
2018-10-18 16:42:27 -0400

    Create AddDonation activity

---
+++ app/src/main/java/team58/cs2340/donationtracker/Models/AddDonation.java
@@ -0,0 +1,15 @@
+package team58.cs2340.donationtracker.Models;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import team58.cs2340.donationtracker.R;
+
+public class AddDonation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_donation);
+    }
+}
