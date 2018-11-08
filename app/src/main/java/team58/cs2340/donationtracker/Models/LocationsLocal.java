45f8fad330aa5dcb555a15b8393cac9a3ccd8e21
Kunal Patel <kunalp1025@gmail.com>
2018-11-08 15:48:00 -0500

    Add All Locations default back to locationsLocal

--- app/src/main/java/team58/cs2340/donationtracker/Models/LocationsLocal.java
+++ app/src/main/java/team58/cs2340/donationtracker/Models/LocationsLocal.java
@@ -1,5 +1,6 @@
 package team58.cs2340.donationtracker.Models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LocationsLocal {
@@ -7,6 +8,13 @@ public class LocationsLocal {
     private static final LocationsLocal instance = new LocationsLocal();
 
     private List<Location> locations;
+
+    private static final Location defaultAllLocation = new Location(-1, "All Locations", "", "",
+            "", "", "", "", null, "", "");
+
+    public LocationsLocal() {
+        this.locations = new ArrayList<>();
+    }
 
     public static LocationsLocal getInstance() {
         return instance;
@@ -18,5 +26,9 @@ public class LocationsLocal {
 
     public void addLocation(Location location) {
         locations.add(location);
+    }
+
+    public static Location getDefaultAllLocation() {
+        return defaultAllLocation;
     }
 }
