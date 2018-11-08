09219db1c9b1c9c628e55f709c987e438fa36fc8
iecurius <saahilyechuri@gmail.com>
2018-11-08 18:45:02 -0500

    Manifest updated with apache + gmaps libraries

--- app/src/main/java/team58/cs2340/donationtracker/Models/LocationsLocal.java
+++ app/src/main/java/team58/cs2340/donationtracker/Models/LocationsLocal.java
@@ -1,5 +1,6 @@
 package team58.cs2340.donationtracker.Models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LocationsLocal {
@@ -7,6 +8,17 @@ public class LocationsLocal {
     private static final LocationsLocal instance = new LocationsLocal();
 
     private List<Location> locations;
+
+    private LocationsLocal() {
+        locations = new ArrayList<>();
+    }
+
+    private static final Location defaultAllLocation = new Location(-1, "All Locations", "", "",
+            "", "", "", "", null, "", "");
+
+    public Location getDefaultAllLocation() {
+        return defaultAllLocation;
+    }
 
     public static LocationsLocal getInstance() {
         return instance;
