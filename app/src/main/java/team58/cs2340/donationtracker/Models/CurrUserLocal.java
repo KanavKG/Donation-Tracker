d6f25efcbbeb63dd429973ca28d36b41933a4122
KanavG <kanav2garg@gmail.com>
2018-11-09 22:14:51 -0500

    Javadocs!

--- app/src/main/java/team58/cs2340/donationtracker/Models/CurrUserLocal.java
+++ app/src/main/java/team58/cs2340/donationtracker/Models/CurrUserLocal.java
@@ -5,18 +5,33 @@ public class CurrUserLocal {
 
     private User currentUser;
 
+    /**
+     * Function to return curruserlocal
+     * @return instance of curruser
+     */
     public static CurrUserLocal getInstance() {
         return instance;
     }
 
+    /**
+     * Function to return currentuser
+     * @return currentUser
+     */
     public User getCurrentUser() {
         return currentUser;
     }
 
+    /**
+     * Function to set current user
+     * @param currentUser current user
+     */
     public void setCurrentUser(User currentUser) {
         this.currentUser = currentUser;
     }
 
+    /**
+     * Function to clear current user
+     */
     public void clearCurrentUser() {
         this.currentUser = null;
     }
