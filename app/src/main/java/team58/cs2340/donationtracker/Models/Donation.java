6a05af26c853bc52c37492cd98487ae95b750e90
KanavG <kanav2garg@gmail.com>
2018-11-09 22:26:13 -0500

    Minor changes

--- app/src/main/java/team58/cs2340/donationtracker/Models/Donation.java
+++ app/src/main/java/team58/cs2340/donationtracker/Models/Donation.java
@@ -38,86 +38,173 @@ public class Donation implements Serializable{
         this(new Date(), name, location, value, shortDescription, fullDescription, category, comment);
     }
 
+    /**
+     * Function to check category
+     * @param category category to be checked
+     * @return boolean value of check
+     */
     public boolean checkCategory(Category category) {
         return this.category.equals(category);
     }
 
+    /**
+     * Function to check location
+     * @param location location to be checked
+     * @return boolean value of check
+     */
     public boolean checkLocation(Location location) {
         return this.location.equals(location);
     }
 
+    /**
+     * Function to check location
+     * @param name name to be checked
+     * @return boolean value of check
+     */
     public boolean checkName(String name) {
         return this.name.equals(name);
     }
 
+    /**
+     * Function to get time stamp
+     * @return current time stamp
+     */
     public Date getTimeStamp() {
         return timeStamp;
     }
 
+    /**
+     * Function to set time stamp
+     * @param timeStamp current time stamp
+     */
     public void setTimeStamp(Date timeStamp) {
         this.timeStamp = timeStamp;
     }
 
+    /**
+     * Function to get location
+     * @return current location
+     */
     public String getLocation() {
         return location;
     }
 
+    /**
+     * Function to set location
+     * @param location location to be set
+     */
     public void setLocation(String location) {
         this.location = location;
     }
 
+    /**
+     * Function to get name
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Function to set name
+     * @param name name to be set
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Function to get short description
+     * @return short description
+     */
     public String getShortDescription() {
         return shortDescription;
     }
 
+    /**
+     * Function to set short description
+     * @param shortDescription to be set
+     */
     public void setShortDescription(String shortDescription) {
         this.shortDescription = shortDescription;
     }
 
+    /**
+     * Function to get full description
+     * @return full description of item
+     */
     public String getFullDescription() {
         return fullDescription;
     }
 
+    /**
+     * Function to set full description
+     * @param fullDescription to be set
+     */
     public void setFullDescription(String fullDescription) {
         this.fullDescription = fullDescription;
     }
 
+    /**
+     * Function to get value of item
+     * @return value of the item
+     */
     public double getValue() {
         return value;
     }
 
+    /**
+     * Function to set value
+     * @param value value of item
+     */
     public void setValue(double value) {
         this.value = value;
     }
 
+    /**
+     * Function to get category of item
+     * @return category of item
+     */
     public Category getCategory() {
         return category;
     }
 
+    /**
+     * Function to set category of item
+     * @param category to be set
+     */
     public void setCategory(Category category) {
         this.category = category;
     }
 
+    /**
+     * Function to get comment
+     * @return comment
+     */
     public String getComment() {
         return comment;
     }
 
+    /**
+     * Function to set comment
+     * @param comment comment to be set
+     */
     public void setComment(String comment) {
         this.comment = comment;
     }
 
+    /**
+     * Function to get photo
+     * @return photo to be returned
+     */
     public Bitmap getPhoto() {
         return photo;
     }
 
+    /**
+     * Function to set photo
+     * @param photo photo to be set
+     */
     public void setPhoto(Bitmap photo) {
         this.photo = photo;
     }
